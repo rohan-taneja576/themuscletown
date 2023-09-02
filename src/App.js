394150d8b1d6d68a14daeb5d800d74ad8e0c0849
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-09-02 17:41:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Join from "./components/Join/Join";
+import Plans from "./components/Plans/Plans";
+import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
+import Testimonals from "./components/Testimonals/Testimonals";
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonals />
+      <Join />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,52 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./Join.css";
+
+const Join = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_USER_ID"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user-email"
+            placeholder="Enter your Email address"
+          />
+          <button className="btn btn-j">Join Now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
