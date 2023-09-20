57cdec45f5b854a8131dd3979635b0f540b0d904
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-09-20 15:03:21 +0530

    update the social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -57,7 +57,7 @@ const Footer = () => {
             </div>
             <div className="phone">
               <AiOutlinePhone fontSize={"2rem"} />
-              <div className="text">8930042241</div>
+              <div className="text">893-004-2241</div>
             </div>
             <div className="address">
               <AiOutlineEnvironment fontSize={"2rem"} />
